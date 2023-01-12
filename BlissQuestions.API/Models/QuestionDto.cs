1402014048aec78b76d2ab7d1b0686157f388dcb
Nuno Teixeira <nunowallenstein@gmail.com>
2023-01-12 11:07:05 +0000

    EntityFramework code first added

---
+++ BlissQuestions.API/Models/QuestionDto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BlissQuestions.API.Models
+{
+    public class QuestionDto
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public string? ImageUrl { get; set; }
+
+        public string? ThumbUrl { get; set; }
+
+        [Required]
+        public string? Question { get; set; }
+
+        public DateTime PublishedAt { get; set; }
+
+        public ICollection<ChoiceDto> Choices { get; set; } = new List<ChoiceDto>();
+
+    }
+}
